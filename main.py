8e5a4684062ba0de982587ebfce27d79db459507
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-05-05 12:13:01 +0200

    add logic to open page

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
 
-def get_source_html():
-    pass
+def get_source_html(url):
+
+    driver=webdriver.Chrome(
+        executable_path="chromedriver/chromedriver.exe"
+    )
+    driver.maximize_window()
+
+    try:
+        driver.get(url=url)
+        time.sleep(3)
+    except Exception as _ex:
+        print(_ex)
+    finally:
+        driver.close()
+        driver.quit()
 
 def main():
-    get_source_html()
+    get_source_html(url='https://careerspace.app/')
 
 if __name__=="__main__":
     main()
